25232903811a010b7a193b9331a7509e90d4eaa3
archyrysss <archyrysss@gmail.com>
2021-05-19 06:53:40 +0300

    AIUContractFinder_alpha.3

---
+++ AIUContractFinder/Operators/TextFinder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIUContractFinder.Operators
+{
+    class TextFinder
+    {
+        private string text;
+
+        public TextFinder(string t)
+        {
+            text = t;
+        }
+
+        public string FindText(string p)
+        {
+            StreamReader SR = new StreamReader(p);
+            string str = SR.ReadLine();
+            while (!string.IsNullOrEmpty(str))
+            {
+                if (str.Contains(text)) return p;
+            }
+            return "";
+        }
+    }
+}
